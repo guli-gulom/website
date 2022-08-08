4873488d173261043501625cd507bca857d2b54f
guli-gulom <guli.gulom1254@gmail.com>
2022-08-08 08:27:28 +0200

    created react app

---
+++ src/Second.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Second() {
+  return (
+    <>
+    <div className="container">
+        <div className="wrapper">
+            
+        </div>
+    </div>
+    </>
+  )
+}
